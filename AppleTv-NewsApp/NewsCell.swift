905aeb9c60d88a669fa4b073034982b38295536f
Abhilash Knox George <user.abhilash@gmail.com>
2022-12-15 13:37:24 +0530

    completed project 3

--- AppleTv-NewsApp/NewsCell.swift
+++ AppleTv-NewsApp/NewsCell.swift
@@ -11,7 +11,7 @@ class NewsCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var unfocussedConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var unfocussedConstraint: NSLayoutConstraint!
     
 //MARK: Move the text view slightly down to match with the image view scalling
 
@@ -23,11 +23,11 @@ class NewsCell: UICollectionViewCell {
 //        focussedConstraint = textLabel.topAnchor.constraint(equalTo: imageView.focusedFrameGuide.bottomAnchor, constant: 15)
 //    }
 
-//    override func updateConstraints() {
-//        super.updateConstraints()
-//        focussedConstraint.isActive = isFocused
-//        unfocussedConstraint.isActive = !isFocused
-//    }
+    override func updateConstraints() {
+        super.updateConstraints()
+      //  focussedConstraint.isActive = isFocused
+     //   unfocussedConstraint.isActive = !isFocused
+    }
 
     override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         super.didUpdateFocus(in: context, with: coordinator)
--- AppleTv-NewsApp/ViewController.swift
+++ AppleTv-NewsApp/ViewController.swift
@@ -8,19 +8,47 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
+    
+    var apiKey = "1a6e0805-4635-4eb7-861a-a727235b78bf"
+    var articles = [JSON]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        guard let title = title else {return}
+        guard let url = URL(string: "https://content.guardianapis.com/\(title.lowercased())?api-key=\(apiKey)&show-feilds=thumbnail,headline,standfirst,body") else {return}
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.fetch(url)
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return articles.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        return cell
+        guard let newCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? NewsCell else { fatalError("Couldn't deque a cell!")}
+        let newsItem = articles[indexPath.row]
+        let title = newsItem["fields"]["headline"].stringValue
+        let thumbnail = newsItem["fields"]["thumbnail"].stringValue
+        
+        newCell.textLabel.text  = title
+        
+        return newCell
+    }
+    
+    func fetch(_ url: URL) {
+        if let data = try? Data(contentsOf: url) {
+            articles = JSON(data)["response"]["results"].arrayValue
+            
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        } else {
+            debugPrint("There has been an error! Try solving it on your own as I don't know what is to be done with if no response is recieved!!")
+        }
     }
 
 }
